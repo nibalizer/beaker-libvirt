abbff0b84e4329b06f4d9628a73521e679e8efe1
Spencer Krum <nibz@spencerkrum.com>
2015-03-25 02:19:59 -0700

    broken tests, yolo

---
+++ spec/beaker/hypervisor/libvirt_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+module Beaker
+  describe Libvirt do
+    let( :options ) { make_opts.merge({ 'logger' => double().as_null_object }) }
+    let( :libvirt ) { Beaker::Libvirt.new( @hosts, options ) }
+
+    before :each do
+      @hosts = make_hosts( {
+        :cloud_provider => 'DigitalOcean',
+        :user => 'vagrant',
+        :password => 'vagrant',
+        :qcow2 => '/home/nibz/projects/pcci/nocloudprimaryubuntu.qcow2',
+        :private_key_file => '/home/nibz/sandbox/vagrant_private.key'
+        }
+      )
+    end
+
+    it "can provision a set of hosts" do
+      ::Libvirt.mock!
+      allow(File).to receive(:read).with('/home/nibz/sandbox/vagrant_private.key').and_return('Key')
+      libvirt.provision
+    end
+
+  end
+end
